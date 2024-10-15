28388506366039625671247e0a831bd7e4a7dcc4
Suvit Asnani <168644149+suvitasnani@users.noreply.github.com>
2024-10-15 13:06:59 -0400

    gif background

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     const gif = document.getElementById("bgGif");
     const gifs = [
-        "https://media3.giphy.com/media/c76IJLufpNwSULPk77/giphy.gif?cid=6c09b952eefdi88tf7evj3m9jo67c1644ndd04ooh32xo7q4&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=g",
-        "https://media.tenor.com/-Y2YOay3_JoAAAAM/its-friday-dancing.gif",
+        "img/1.gif",
+        "img/2.gif",
+        "img/3.gif",
+        "img/4.gif",
+        "img/5.gif",
+        "img/6.gif",
+        "img/7.gif",
+        "img/8.gif",
     ];
     let gifIndex = 0;
 
@@ -12,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     gif.addEventListener("load", function() {
-        setTimeout(showNextGif, 7000); // Change GIF every 5 seconds
+        setTimeout(showNextGif, 6000); // Change GIF every 5 seconds
     });
 
     showNextGif();
