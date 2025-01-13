ebecfea945d7bc8968f3a90dd3744c09b44d15b8
remander <remymander@gmail.com>
2025-01-12 22:31:23 -0500

    Fixed the overlay after the alert.
    An overlay was causing the entire screen to be clouded after the alert popup disappeared.

--- js/alert.js
+++ js/alert.js
@@ -1,9 +1,16 @@
-
 // File: alert.js
-// Description: This file contains the JavaScript code for the modal that pops off when loading the index.html  
+// Description: This file contains the JavaScript code for the modal that pops off when loading the index.html
 
+document.addEventListener("DOMContentLoaded", function () {
+  // Wait for the document to be fully loaded
+  var myModal = new bootstrap.Modal(document.getElementById("exampleModal")); // Create a new instance of the modal
+  myModal.show(); // Show the modal
 
-   document.addEventListener("DOMContentLoaded", function () {                   // Wait for the document to be fully loaded
-     var myModal = new bootstrap.Modal(document.getElementById("exampleModal")); // Create a new instance of the modal
-     myModal.show();                                                             // Show the modal
-   });
+  // Remove the overlay when the modal is closed
+  document.getElementById("exampleModal").addEventListener("hidden.bs.modal", function () {
+    var modalBackdrop = document.querySelector(".modal-backdrop");
+    if (modalBackdrop) {
+      modalBackdrop.remove();
+    }
+  });
+});
