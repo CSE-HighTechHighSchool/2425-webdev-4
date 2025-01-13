7cfe67d72ffe4c83573f39ace75846b614f05715
remander <remymander@gmail.com>
2025-01-12 21:29:23 -0500

    Fixed dataViewer navbar

--- js/alert.js
+++
@@ -1,9 +0,0 @@
-/*
-File: alert.js
-Description: This file contains the JavaScript code for the modal that pops off when loading the index.html  
-*/
-
-  // document.addEventListener("DOMContentLoaded", function () {                   // Wait for the document to be fully loaded
-  //   var myModal = new bootstrap.Modal(document.getElementById("exampleModal")); // Create a new instance of the modal
-  //   myModal.show();                                                             // Show the modal
-  // });
